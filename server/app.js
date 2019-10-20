23237d36fb4cdae89058ebc75cf5306a004c046e
Videcte <videcte@gmail.com>
2019-10-21 00:28:34 +0200

    initial

---
+++ server/app.js
@@ -0,0 +1,25 @@
+import mongoose from 'mongoose';
+import express from 'express';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
+import morgan from 'morgan';
+import cors from 'cors';
+
+const app = express();
+const host = '127.0.0.1';
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+app.use(morgan('tiny'));
+app.use(helmet() , () =>{
+    console.log('Helmet ON HEAD !');
+})
+app.set('port' , 3000);
+let server = app.listen(app.get('port') , () => {
+    console.log(`Server working on port ' ${host} ${server.address().port}`);
+});
+
+
+
